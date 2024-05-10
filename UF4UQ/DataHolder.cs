7f6a5ee7862fa3642f285f9f70160a1c7087e03a
andrewpolar <61638749+andrewpolar@users.noreply.github.com>
2024-05-10 14:56:36 -0400

    Add files via upload

---
+++ UF4UQ/DataHolder.cs
@@ -0,0 +1,359 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace UQ
+{
+    interface IDataGenerator
+    {
+        public double Formula(double[] x);
+        public double[] GetRandomInput();
+        public double[] AddNoise(double[] x, double magnitude, out double datanorm, out double errornorm);
+    }
+
+    class DataGenerator3: IDataGenerator
+    {
+        Random _rnd = new Random();
+
+        public double Formula(double[] x)
+        {
+            double ax = x[0] - x[2];
+            double ay = x[1] - x[3];
+            double a = Math.Sqrt(ax * ax + ay * ay);
+            if (a < 5.0) return -1.0;
+
+            double bx = x[0] - x[4];
+            double by = x[1] - x[5];
+            double b = Math.Sqrt(bx * bx + by * by);
+            if (b < 5.0) return -1.0;
+
+            double cx = x[2] - x[4];
+            double cy = x[3] - x[5];
+            double c = Math.Sqrt(cx * cx + cy * cy);
+            if (c < 5.0) return -1.0;
+
+            if (c >= a && c >= b)
+            {
+                if ((a + b) / c < 1.2) return -1.0;
+            }
+
+            if (a >= b && a >= c)
+            {
+                if ((b + c) / a < 1.2) return -1.0;
+            }
+
+            if (b >= c && b >= a)
+            {
+                if ((c + a) / b < 1.2) return -1.0;
+            }
+
+            double s = (a + b + c) / 2.0;
+            double d = (s - a) * (s - b) * (s - c);
+            if (d < 0.0)
+            {
+                Console.WriteLine("Failed to compute area {0:0.0000}, {1:0.0000}, {2:0.0000}, {3:0.0000}", a, b, c, d);
+                Environment.Exit(0);
+            }
+            double Area = Math.Sqrt(s * d);
+            return Area; 
+        }
+        public double[] GetRandomInput()
+        {
+            double Area = -1.0;
+            int N = 6;
+            double[] x = new double[N];
+            while (true)
+            {
+                for (int i = 0; i < N; ++i)
+                {
+                    x[i] = _rnd.Next() % 100;
+                }
+
+                Area = Formula(x);
+                if (Area > 10.0) break;
+            }
+            return x;
+        }
+
+        public double[] AddNoise(double[] x, double magnitude, out double datanorm, out double errornorm)
+        {
+            datanorm = 1.0;
+            errornorm = 0.0;
+            return x;
+        }
+    }
+
+    class DataGenerator1: IDataGenerator
+    {
+        Random _rnd = new Random();
+
+        public double Formula(double[] x)
+        {
+            //y = (1/pi)*(2+2*x3)*(1/3)*(atan(20*exp(x5)*(x1-0.5+x2/6))+pi/2) + (1/pi)*(2+2*x4)*(1/3)*(atan(20*exp(x5)*(x1-0.5-x2/6))+pi/2);
+            double pi = 3.14159265359;
+            if (5 != x.Length)
+            {
+                Console.WriteLine("Formala error");
+                Environment.Exit(0);
+            }
+            double y = (1.0 / pi);
+            y *= (2.0 + 2.0 * x[2]);
+            y *= (1.0 / 3.0);
+            y *= Math.Atan(20.0 * Math.Exp(x[4]) * (x[0] - 0.5 + x[1] / 6.0)) + pi/2.0;
+
+            double z = (1.0 / pi);
+            z *= (2.0 + 2.0 * x[3]);
+            z *= (1.0 / 3.0);
+            z *= Math.Atan(20.0 * Math.Exp(x[4]) * (x[0] - 0.5 - x[1] / 6.0)) + pi/2.0;
+ 
+            return y + z;
+        }
+
+        public double[] GetRandomInput()
+        {
+            double[] x = new double[5];
+            x[0] = (_rnd.Next() % 100) / 100.0;
+            x[1] = (_rnd.Next() % 100) / 100.0;
+            x[2] = (_rnd.Next() % 100) / 100.0;
+            x[3] = (_rnd.Next() % 100) / 100.0;
+            x[4] = (_rnd.Next() % 100) / 100.0;
+            return x;
+        }
+
+        public double[] AddNoise(double[] x, double magnitude, out double datanorm, out double errornorm)
+        {
+            double[] z = new double[5];
+            z[0] = x[0] + magnitude * ((_rnd.Next() % 100) / 100.0 - 0.5);
+            z[1] = x[1] + magnitude * ((_rnd.Next() % 100) / 100.0 - 0.5);
+            z[2] = x[2] + magnitude * ((_rnd.Next() % 100) / 100.0 - 0.5);
+            z[3] = x[3] + magnitude * ((_rnd.Next() % 100) / 100.0 - 0.5);
+            z[4] = x[4] + magnitude * ((_rnd.Next() % 100) / 100.0 - 0.5);
+
+            datanorm = 0.0;
+            foreach (double d in x)
+            {
+                datanorm += d * d;
+            }
+            datanorm /= x.Length;
+            datanorm = Math.Sqrt(datanorm);
+
+            errornorm = 0.0;
+            for (int i = 0; i < x.Length; ++i)
+            {
+                errornorm += (x[i] - z[i]) * (x[i] - z[i]);
+            }
+            errornorm /= x.Length;
+            errornorm = Math.Sqrt(errornorm);
+
+            return z;
+        }
+    }
+
+    class DataGenerator2: IDataGenerator
+    {
+        Random _rnd = new Random();
+
+        public double Formula(double[] x)
+        {
+            if (5 != x.Length)
+            {
+                Console.WriteLine("Formala error");
+                Environment.Exit(0);
+            }
+            double y = Math.Abs(Math.Pow(Math.Sin(x[1]), x[0]) - 1.0 / Math.Exp(x[2])) / x[3] + x[4] * Math.Cos(x[4]);
+            return y;
+        }
+
+        public double[] GetRandomInput()
+        {
+            double[] x = new double[5];
+            x[0] = (_rnd.Next() % 100) / 100.0;
+            x[1] = (_rnd.Next() % 100) / 100.0 * 3.14 / 2.0;
+            x[2] = (_rnd.Next() % 50) / 100.0 + 1;
+            x[3] = (_rnd.Next() % 100) / 100.0 + 0.4;
+            x[4] = (_rnd.Next() % 100) / 200.0;
+            return x;
+        }
+
+        public double[] AddNoise(double[] x, double magnitude, out double datanorm, out double errornorm)
+        {
+            double[] xx = new double[x.Length];
+            for (int j = 0; j < x.Length; ++j)
+            {
+                xx[j] = x[j] + _rnd.Next() % 100 / 100.0 * magnitude - magnitude / 2.0;
+            }
+            if (xx[0] < 0.0) xx[0] = 0.0;
+            if (xx[1] < 0.0) xx[1] = 0.0;
+            if (xx[2] < 1.0) xx[2] = 1.0;
+            if (xx[3] < 0.4) xx[3] = 0.4;
+            if (xx[4] < 0.0) xx[4] = 0.0;
+
+            if (xx[0] > 1.0) xx[0] = 1.0;
+            if (xx[1] > 3.14 / 2.0) xx[1] = 3.14 / 2.0;
+            if (xx[2] > 1.5) xx[2] = 1.5;
+            if (xx[3] > 1.4) xx[3] = 1.4;
+            if (xx[4] > 0.5) xx[4] = 0.5;
+
+            datanorm = 0.0;
+            foreach (double d in x)
+            {
+                datanorm += d * d;
+            }
+            datanorm /= x.Length;
+            datanorm = Math.Sqrt(datanorm);
+
+            errornorm = 0.0;
+            for (int i = 0; i < x.Length; ++i)
+            {
+                errornorm += (x[i] - xx[i]) * (x[i] - xx[i]);
+            }
+            errornorm /= x.Length;
+            errornorm = Math.Sqrt(errornorm);
+
+            return xx;
+        }
+    }
+
+    class DataHolder
+	{
+        public List<double[]> _inputs = new List<double[]>();
+        public List<double> _target = new List<double>();
+        public List<int> _id = new List<int>();
+        public double[] _xmin = null;
+        public double[] _xmax = null;
+        public double _targetMin;
+        public double _targetMax;
+        public double _noise = 0.0;
+        private IDataGenerator iData = new DataGenerator2(); //this is switch between different datasets
+
+        public double[] GetRandomInput()
+        {
+            return iData.GetRandomInput();
+        }
+
+        public double GetExactOutput(double[] input)
+        {
+            return iData.Formula(input);
+        }
+
+        public void ShowInputs(int category)
+        {
+            int N = _inputs.Count;
+            for (int i = 0; i < N; ++i)
+            {
+                if (category == _id[i])
+                {
+                    foreach (double d in _inputs[i])
+                    {
+                        Console.Write("{0:0.00} ", d);
+                    }
+                    Console.Write(" | {0:0.00} ", _target[i]);
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public double[] GetStatData(double[] x, int N)
+        {
+            double[] y = new double[N];
+
+            for (int i = 0; i < N; ++i)
+            {
+                double datanorm;
+                double errornorm;
+                y[i] = iData.Formula(iData.AddNoise(x, _noise, out datanorm, out errornorm));
+            }
+
+            return y;
+        }
+
+        public void BuildFormulaData(double noise, int N)
+        {
+            _noise = noise;
+            _inputs.Clear();
+            _target.Clear();
+
+            double alldatanorm = 0.0;
+            double allerrornorm = 0.0;
+
+            for (int i = 0; i < N; ++i)
+            {
+                double datanorm;
+                double errornorm;
+                double[] x = GetRandomInput();
+                double y = iData.Formula(iData.AddNoise(x, noise, out datanorm, out errornorm));
+
+                alldatanorm += datanorm;
+                allerrornorm += errornorm;
+              
+                _inputs.Add(x);
+                _target.Add(y);
+                _id.Add(0);
+            }
+            FindMinMax();
+
+            Console.WriteLine("Data is generated, average relative error {0:0.0000}", allerrornorm / alldatanorm);
+        }
+
+        private void FindMinMax()
+        {
+            int size = _inputs[0].Length;
+            _xmin = new double[size];
+            _xmax = new double[size];
+
+            for (int i = 0; i < size; ++i)
+            {
+                _xmin[i] = double.MaxValue;
+                _xmax[i] = double.MinValue;
+            }
+
+            for (int i = 0; i < _inputs.Count; ++i)
+            {
+                for (int j = 0; j < _inputs[i].Length; ++j)
+                {
+                    if (_inputs[i][j] < _xmin[j]) _xmin[j] = _inputs[i][j];
+                    if (_inputs[i][j] > _xmax[j]) _xmax[j] = _inputs[i][j];
+                }
+
+            }
+
+            _targetMin = double.MaxValue;
+            _targetMax = double.MinValue;
+            for (int j = 0; j < _target.Count; ++j)
+            {
+                if (_target[j] < _targetMin) _targetMin = _target[j];
+                if (_target[j] > _targetMax) _targetMax = _target[j];
+            }
+        }
+
+        public double[] GetInputMin()
+        {
+            return _xmin;
+        }
+
+        public double[] GetInputMax()
+        {
+            return _xmax;
+        }
+
+        public double GetTargetMax()
+        {
+            return _targetMax;
+        }
+
+        public double GetTargetMin()
+        {
+            return _targetMin;
+        }
+
+        public int GetNumberOfInputs()
+        {
+            double[] sample = _inputs[0];
+            return sample.Length;
+        }
+    }
+}
+
+
+
---
+++ UF4UQ/PLL.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UQ
+{
+    [Serializable]
+    class PLL
+    {
+        private int _points;
+        private double[] _x;
+        private double[] _y;
+        private double _deltax;
+        private double _xmin = double.MaxValue;
+        private double _xmax = double.MinValue;
+
+        const double _zet = 0.0005;
+        private double _limm;
+        private double _limv;
+
+        public PLL(int points, double xmin, double xmax)
+        {
+            Initialize(points, xmin, xmax);
+        }
+
+        public void SetConstant(double c)
+        {
+            for (int i = 0; i < _points; ++i)
+            {
+                _y[i] = c;
+            }
+        }
+
+        public void SetLinear(double ymin, double ymax)
+        {
+            double deltay = (ymax - ymin) / (_points - 1);
+            for (int i = 0; i < _points; ++i)
+            {
+                _y[i] = ymin + deltay * i;
+            }
+        }
+
+        public void SetRandom(double ymin, double ymax)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < _points; ++i)
+            {
+                _y[i] = (rnd.Next() % 100) / 100.0 * (ymax - ymin) + ymin;
+            }
+        }
+
+        public double GetDerrivative(double x)
+        {
+            int low = (int)((x - _x[0]) / _deltax);
+            if (low < 0) low = 0;
+            if (low > _points - 2) low = _points - 2;
+            return (_y[low + 1] - _y[low]) / _deltax;
+        }
+
+        private void Initialize(int points, double xmin, double xmax)
+        {
+            if (points < 2)
+            {
+                Console.WriteLine("Number of blocks is too low");
+                Environment.Exit(0);
+            }
+            if (xmin >= xmax)
+            {
+                Console.WriteLine("The limits are invalid {0:0.000}, {1:0.0000}", xmin, xmax);
+                Environment.Exit(0);
+            }
+            _points = points;
+            _x = new double[_points];
+            _y = new double[_points];
+            _deltax = (xmax - xmin) / (_points - 1);
+            for (int i = 0; i < _points; ++i)
+            {
+                _x[i] = xmin + i * _deltax;
+                _y[i] = 0.0;
+            }
+            _limm = (xmax + xmin) / 2.0;
+            _limv = (xmax - xmin) * (xmax - xmin) / 12.0;
+        }
+
+        public void Resize(int N)
+        {
+            double[] tmp = new double[N];
+            double deltax = (_x[_points - 1] - _x[0]) / (N - 1);
+            double min = _x[0];
+            for (int i = 0; i < N; ++i)
+            {
+                double x = min + i * deltax;
+                tmp[i] = GetFunction(x);
+            }
+
+            _x = new double[N];
+            _y = new double[N];
+            _deltax = deltax;
+            for (int i = 0; i < N; ++i)
+            {
+                _x[i] = min + i * _deltax;
+                _y[i] = tmp[i];
+            }
+            _points = N;
+        }
+
+        private void Renormalize(double xmin, double xmax, bool isBottom)
+        {
+            _deltax = (xmax - xmin) / (_points - 1);
+            if (isBottom)
+            {
+                for (int i = 0; i < _x.Length; ++i)
+                {
+                    _x[i] = xmin + i * _deltax;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < _x.Length; ++i)
+                {
+                    _x[_x.Length - 1 - i] = xmax - i * _deltax;
+                }
+            }
+        }
+
+        public void Update(double x, double delta, double mu, bool isLeaf)
+        {
+            if (x < _xmin) _xmin = x;
+            if (x > _xmax) _xmax = x;
+
+            if (x < _x[0])
+            {
+                Renormalize(x, _x[_x.Length - 1], true);
+            }
+
+            if (x > _x[_x.Length - 1])
+            {
+                Renormalize(_x[0], x, false);
+            }
+
+            int left = (int)((x - _x[0]) / _deltax);
+            if (left < 0) left = 0;
+            if (left == _y.Length - 1)
+            {
+                _y[_y.Length - 1] += delta * mu;
+                return;
+            }
+
+            double leftx = x - _x[left];
+            double rightx = _x[left + 1] - x;
+            _y[left + 1] += delta * leftx / _deltax * mu;
+            _y[left] += delta * rightx / _deltax * mu;
+        }
+
+        public double GetFunction(double x)
+        {
+            if (x < _x[0])
+            {
+                double derrivative = (_y[1] - _y[0]) / _deltax;
+                return _y[1] - derrivative * (_x[1] - x);
+            }
+            if (x > _x[_points - 1])
+            {
+                double derrivative = (_y[_points - 1] - _y[_points - 2]) / _deltax;
+                return _y[_points - 2] + derrivative * (x - _x[_points - 2]);
+            }
+
+            int left = (int)((x - _x[0]) / _deltax);
+            if (left < 0) left = 0;
+            if (left > _points - 2) left = _points - 2;
+
+            double leftx = x - _x[left];
+            return (_y[left + 1] - _y[left]) / _deltax * leftx + _y[left];
+        }
+
+        public void ShowData()
+        {
+            for (int i = 0; i < _points; ++i)
+            {
+                Console.WriteLine("{0:0.0000} {1:0.0000}", _x[i], _y[i]);
+            }
+        }
+
+        public void PrintMinMax()
+        {
+            Console.WriteLine("PLL xmin-layer={0:0.0000} xmax-layer={1:0.0000}", _x[0], _x[_points - 1]);
+        }
+
+        public double GetY(int pos)
+        {
+            if (pos > _points - 1) pos = _points - 1;
+            if (pos < 0) pos = 0;
+            return _y[pos];
+        }
+
+        public int GetNumberOfPoints()
+        {
+            return _points;
+        }
+
+        public double GetXmin()
+        {
+            return _x[0];
+        }
+
+        public double GetXmax()
+        {
+            return _x[_points - 1];
+        }
+
+        public double[] GetAllFunc()
+        {
+            return _y;
+        }
+
+        public double[] GetAllArgs()
+        {
+            return _x;
+        }
+
+        public double GetDeltax()
+        {
+            return _deltax;
+        }
+
+        public double GetXMean()
+        {
+            return _limm;
+        }
+
+        public double GetXVar()
+        {
+            return _limv;
+        }
+
+        public int GetNPoints()
+        {
+            return _points;
+        }
+    }
+}
